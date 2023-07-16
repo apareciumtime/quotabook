bae899d8ad4dd8af81cc923ebb09057079d67e4c
apareciumtime <78253494+apareciumtime@users.noreply.github.com>
2023-07-16 23:46:14 +0700

    Home Structure

--- quotabook/resources/views/components/header.blade.php
+++ quotabook/resources/views/components/header.blade.php
@@ -13,11 +13,22 @@
         a{
             text-decoration: none;
         }
+        .header_box_guest{
+            display:flex;
+            height: 96px;
+            padding: 0px 32px;
+            align-items: center;
+            justify-content: center;
+            background-color: #F0F0F0;
+            gap:32px;
+            min-width: fit-content;
+        }
 
         .header_box{
             display:flex;
             padding: 4px 32px;
             align-items: center;
+            justify-content: center;
             background-color: #F0F0F0;
             gap:32px;
             min-width: fit-content;
@@ -121,13 +132,43 @@
             display: flex;
             flex-direction: column;
             align-items: center;
-            min-width: 90vw;
-            /* border: 1px blue solid; */
+        }
+
+        .welcoming{
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex: 1 0 0;
+            /* align-self: stretch; */
+
+            color: var(--click, #CAC0A8);
+            font-size: 24px;
+            /* border:1px red solid; */
         }
     </style>
 </head>
 <body>
-    
+    @if (Auth::guest()) 
+    <div class="header_box_guest">
+        <a href="/">
+            <div class="header_box_logo">
+                <svg xmlns="http://www.w3.org/2000/svg" width="64" height="64" viewBox="0 0 64 64" fill="none">
+                    <path d="M64 32C64 49.6731 49.6731 64 32 64C14.3269 64 0 49.6731 0 32C0 14.3269 14.3269 0 32 0C49.6731 0 64 14.3269 64 32Z" fill="#CAC0A8"/>
+                    <path d="M42.888 50.44C41.896 50.44 40.984 50.2 40.152 49.72C39.32 49.24 38.648 48.6 38.136 47.8C35.96 48.888 33.608 49.432 31.08 49.432C28.456 49.432 26.056 48.904 23.88 47.848C21.736 46.792 20.04 45.368 18.792 43.576C17.544 41.784 16.92 39.816 16.92 37.672V26.68C16.92 24.536 17.528 22.568 18.744 20.776C19.992 18.984 21.688 17.576 23.832 16.552C26.008 15.496 28.424 14.968 31.08 14.968C33.736 14.968 36.136 15.496 38.28 16.552C40.456 17.576 42.152 18.984 43.368 20.776C44.616 22.568 45.24 24.536 45.24 26.68V37.672C45.24 39.112 44.952 40.488 44.376 41.8C43.832 43.08 43.032 44.2 41.976 45.16C42.424 45.768 43.016 46.232 43.752 46.552C44.52 46.84 45.32 46.952 46.152 46.888C46.152 48.104 45.88 49 45.336 49.576C44.824 50.152 44.008 50.44 42.888 50.44ZM38.424 40.696C39.128 39.896 39.48 38.872 39.48 37.624V26.776C39.48 24.728 38.712 23.096 37.176 21.88C35.672 20.632 33.64 20.008 31.08 20.008C28.52 20.008 26.472 20.632 24.936 21.88C23.432 23.096 22.68 24.728 22.68 26.776V35.32C23.928 34.904 25.112 34.696 26.232 34.696C28.824 34.696 31.064 35.144 32.952 36.04C34.872 36.936 36.696 38.488 38.424 40.696ZM31.08 44.44C32.488 44.44 33.864 44.2 35.208 43.72C33.896 41.832 32.552 40.568 31.176 39.928C29.8 39.288 28.056 38.968 25.944 38.968C25.112 38.968 24.12 39.144 22.968 39.496C23.448 41 24.392 42.2 25.8 43.096C27.24 43.992 29 44.44 31.08 44.44Z" fill="white"/>
+                </svg>
+            </div>
+        </a>
+        <div class="welcoming">ยินดีต้อนรับสู่ QuotabooK</div>
+        <a href="{{ url('auth/google') }}">
+            <div class="menu">
+                ลงชื่อเข้าใช้
+            </div>
+        </a>
+    </div>
+    <div class="body_box">
+        @yield('body')
+    </div>
+    @else
     <div class="header_box">
         <a href="/">
             <div class="header_box_logo">
@@ -137,13 +178,6 @@
                 </svg>
             </div>
         </a>
-    @if (Auth::guest()) 
-    <a href="{{ url('auth/google') }}">
-        <div class="menu">
-            ลงชื่อเข้าใช้
-        </div>
-    </a>
-    @else
         <div class="header_box_menu_list">
             <x-button-menu label="QUOTE"/>
             <x-button-menu label="BOOK SHELF"/>
@@ -176,9 +210,10 @@
             </div>
         </div>
     </div>
-    @endif
-
     <div class="body_box">
         @yield('body')
     </div>
+    @endif
+
+    
 </body>
--- quotabook/resources/views/homepage/home.blade.php
+++ quotabook/resources/views/homepage/home.blade.php
@@ -1,6 +1,341 @@
+<style>
+
+
+.playground{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    gap: 64px;
+    align-self: stretch;
+    padding:32px 128px;
+    /* border: 1px red solid; */
+}
+
+.quotabook{
+    color: var(--click, #CAC0A8);
+    text-align: center;
+
+    font-size: 32px;
+    font-style: normal;
+    font-weight: 800;
+    line-height: normal;
+    /* border: 1px red solid; */
+}
+
+.quoter_today{
+    width: 100%;
+    height: 256px;
+    /* border: 1px red solid; */
+
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 10px;
+    align-self: stretch;
+}
+
+.quoter_today_box{
+    display: flex;
+    width: 400px;
+    padding: 32px;
+    justify-content: center;
+    align-items: center;
+    border-radius: 20px 20px 0px 20px;
+    border: 4px solid var(--click, #CAC0A8);
+    
+    color: var(--font-secondary-black, rgba(14, 14, 14, 0.50));
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 28px;
+    letter-spacing: 0.36px;
+}
+
+.quoter_today_picture{
+    width: 223.79px;
+    height: 256px;
+    /* border: 1px red solid; */
+}
+
+.quoter_today_picture img{
+    width: 100%;
+    height: 100%;
+}
+
+.content{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 64px;
+    align-self: stretch;
+    /* border: 1px red solid; */
+}
+
+.whole_block{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 16px;
+    align-self: stretch;
+    /* border: 1px red solid; */
+}
+
+.topic_block{
+    padding:0px 16px;
+    color: var(--font-primary-black, #0E0E0E);
+    font-size: 24px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+
+    /* border: 1px red solid; */
+}
+
+.block{
+    display: flex;
+    padding: 32px;
+    justify-content: center;
+    align-items: flex-start;
+    gap: 16px;
+    align-self: stretch;
+
+    border-radius: 4px;
+    border: 4px solid var(--click, #CAC0A8);
+
+    /* border: 1px red solid; */
+}
+
+.block_column{
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    gap: 16px;
+    flex: 1 0 0;
+
+    /* border: 1px red solid; */
+}
+
+.block_column_topic{
+    color: var(--font-primary-black, #0E0E0E);
+
+    font-size: 18px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+}
+
+.block_column_list{
+    color: var(--font-secondary-black, rgba(14, 14, 14, 0.50));
+
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    /* text-decoration-line: underline; */
+
+    /* border: 1px red solid; */
+}
+
+.block_column_list_underline{
+    color: var(--font-secondary-black, rgba(14, 14, 14, 0.50));
+
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    text-decoration-line: underline;
+
+    /* border: 1px red solid; */
+}
+
+.block_column_list_each{
+    color: var(--font-secondary-black, rgba(14, 14, 14, 0.50));
+
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+    /* border: 1px red solid; */
+}
+
+</style>
+
+
+
+
 @extends('components.header')
 
 
 @section('body')
+<div class="playground">
+    <div class="quotabook">
+        QuotabooK
+    </div>
+
+    <div class="quoter_today">
+        <div class="quoter_today_box">
+            Bird by bird ค่อย ๆ เขียนถึงนกไปทีละตัว แล้ววันหนึ่งจะสำเร็จเอง ไม่ต้องรีบร้อน
+        </div>
+        <div class="quoter_today_picture">
+            <img src="/image/quoter.png" alt="Image"/>
+        </div>
+    </div>
+    <div class="content">
+        <div class="whole_block">
+            <div class="topic_block">
+                Quote โควต
+            </div>
+            <div class="block">
+                <div class="block_column">
+                    <div class="block_column_topic">
+                        วิธีการใช้งานเบื้องต้น
+                    </div>
+                    <div class="block_column_list">
+                        <div class="block_column_list_each">
+                            1. 
+                        </div>
+                        <div class="block_column_list_each">
+                            2. 
+                        </div>
+                    </div>
+                </div>
+
+                <div class="block_column">
+                    <div class="block_column_topic">
+                        การใช้งานอื่น ๆ
+                    </div>
+                    <div class="block_column_list_underline">
+                        <a href="#">
+                        <div class="block_column_list_each">
+                            1. 
+                        </div>
+                        </a>
+                        <a href="#">
+                        <div class="block_column_list_each">
+                            2. 
+                        </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="whole_block">
+            <div class="topic_block">
+                Book Shelf ชั้นวางหนังสือ
+            </div>
+            <div class="block">
+                <div class="block_column">
+                    <div class="block_column_topic">
+                        วิธีการใช้งานเบื้องต้น
+                    </div>
+                    <div class="block_column_list">
+                        <div class="block_column_list_each">
+                            1. 
+                        </div>
+                        <div class="block_column_list_each">
+                            2. 
+                        </div>
+                    </div>
+                </div>
+
+                <div class="block_column">
+                    <div class="block_column_topic">
+                        การใช้งานอื่น ๆ
+                    </div>
+                    <div class="block_column_list_underline">
+                        <a href="#">
+                        <div class="block_column_list_each">
+                            1. 
+                        </div>
+                        </a>
+                        <a href="#">
+                        <div class="block_column_list_each">
+                            2. 
+                        </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="whole_block">
+            <div class="topic_block">
+                Writing มุมขีดเขียน
+            </div>
+            <div class="block">
+                <div class="block_column">
+                    <div class="block_column_topic">
+                        วิธีการใช้งานเบื้องต้น
+                    </div>
+                    <div class="block_column_list">
+                        <div class="block_column_list_each">
+                            1. 
+                        </div>
+                        <div class="block_column_list_each">
+                            2. 
+                        </div>
+                    </div>
+                </div>
+
+                <div class="block_column">
+                    <div class="block_column_topic">
+                        การใช้งานอื่น ๆ
+                    </div>
+                    <div class="block_column_list_underline">
+                        <a href="#">
+                        <div class="block_column_list_each">
+                            1. 
+                        </div>
+                        </a>
+                        <a href="#">
+                        <div class="block_column_list_each">
+                            2. 
+                        </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="whole_block">
+            <div class="topic_block">
+                Manage Library จัดการห้องสมุด
+            </div>
+            <div class="block">
+                <div class="block_column">
+                    <div class="block_column_topic">
+                        วิธีการใช้งานเบื้องต้น
+                    </div>
+                    <div class="block_column_list">
+                        <div class="block_column_list_each">
+                            1. 
+                        </div>
+                        <div class="block_column_list_each">
+                            2. 
+                        </div>
+                    </div>
+                </div>
+
+                <div class="block_column">
+                    <div class="block_column_topic">
+                        การใช้งานอื่น ๆ
+                    </div>
+                    <div class="block_column_list_underline">
+                        <a href="#">
+                        <div class="block_column_list_each">
+                            1. 
+                        </div>
+                        </a>
+                        <a href="#">
+                        <div class="block_column_list_each">
+                            2. 
+                        </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+</div>
+
 
 @endsection
