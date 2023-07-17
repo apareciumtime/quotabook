1466cc8e0f03ca07f9345b8dcf555dc99a4fc9ac
apareciumtime <78253494+apareciumtime@users.noreply.github.com>
2023-07-17 14:55:11 +0700

    Remove Login Route and ButtonMenuController

--- quotabook/routes/web.php
+++ quotabook/routes/web.php
@@ -28,8 +28,6 @@ Route::get('auth/google/callback', [ProviderController::class,'loginWithGoogle']
 
 Route::get('logout',[ProviderController::class,'logoutWithGoogle']);
 
-Route::get('login', [ButtonMenuController::class, 'getLogin'])->name('login');
-
  
 Route::get('/auth/callback', function () {
     $user = Socialite::driver('google')->user();
